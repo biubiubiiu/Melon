0b1eaa7987555e1930c3f3624710343d4c41aadf
Raymond Wong <1049204910@qq.com>
2021-03-08 10:05:14 +0800

    opt: Initial setup, including: 1. Migrate build logic from groovy to kotlin dsl 2. Adjust application name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.3.72"
+    val kotlin_version = "1.3.72"
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.1.2"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
@@ -18,6 +18,8 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,13 +2,12 @@ rootProject.name = "Melon"
 
 // Include all the existent modules in the project
 rootDir.walk()
-        .maxDepth(1)
-        .filter {
-            it.name != "buildSrc"
-                    && it.isDirectory
-                    && (file("${it.absolutePath}/build.gradle.kts").exists()
-                    || file("${it.absolutePath}/build.gradle").exists())
-        }
-        .forEach {
-            include(":${it.name}")
-        }
+    .maxDepth(1)
+    .filter {
+        it.name != "buildSrc"
+                && it.isDirectory
+                && file("${it.absolutePath}/build.gradle.kts").exists()
+    }
+    .forEach {
+        include(":${it.name}")
+    }
